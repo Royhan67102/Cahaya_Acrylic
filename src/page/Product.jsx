8816f25e01538a55039134bc71795c9155c03f52
Farida964 <imfaravi@gmail.com>
2025-08-21 15:23:43 +0700

    fixing

--- src/page/Product.jsx
+++ src/page/Product.jsx
@@ -3,7 +3,7 @@ import Hero from '../components/Home/Hero/Hero';
 import Sidebar from '../components/Product/Sidebar/Sidebar';
 import Footer from '../components/Footer/Footer';
 import Products from '../components/Product/Products/Products';
-import styles from './product.module.css'; // Assuming you have a CSS module for styling
+import styles from './product02.module.css'; // Assuming you have a CSS module for styling
 
 function Product() {
     return (

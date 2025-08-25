178d795f7e1718503734f6a74708c977e7521f18
Royhan67102 <royhanaudy14@gmail.com>
2025-08-25 17:55:49 +0700

    push

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -11,9 +11,13 @@ function Hero() {
       id: 1,
       text: (
         <>
-          <span className={styles.word1}>ANYTHING YOU WANT</span> ACRYLIC WILL
-          <br />
-          <span className={styles.word1}>MAKE IT HAPPEN</span>
+          <div className={styles.maps_container}> 
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.330466208772!2d106.79227139999999!3d-6.
+               6057939!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c5bfa4f9cab9%3A0x148f7c322c4a5bea!2sJl.
+               %20Pancasan%20No.19%2C%20RT.01%2FRW.07%2C%20Pasir%20Jaya%2C%20Kec.%20Bogor%20Bar.%2C%20Kota%20Bogor%2C%20
+               Jawa%20Barat%2016119!5e0!3m2!1sen!2sid!4v1756114563706!5m2!1sen!2sid" className={styles.maps}></iframe>
+               </div>
+          <span>Jl. Pancasan No.19, Kota Bogor, Jawa Barat</span>
         </>
       ),
       img1: "/crystal1.png",
